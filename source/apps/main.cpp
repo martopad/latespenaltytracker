2a2bee2a449dc8b34dc11c9a4634a695e9fe9def
martopad <martin_luther_yung@outlook.com>
2018-10-06 20:40:12 +0800

    compiles without test, and will cause segfault due to paths not resolved

--- source/apps/main.cpp
+++ source/apps/main.cpp
@@ -13,11 +13,11 @@
 //#include "Sprint.hpp"
 //#include <boost/filesystem.hpp>
 #include <ctime>
-#include "utilities/Date_Operations.hpp"
+#include "../libs/utilities/Date_Operations.hpp"
 // #include "Tests.hpp"
-#include "utilities/File_Accessor.hpp"
-#include "utilities/Regex_Operations.hpp"
-#include "Penalty.hpp"
+#include "../libs/utilities/File_Accessor.hpp"
+#include "../libs/utilities/Regex_Operations.hpp"
+#include "../libs/Penalty.hpp"
 
 
 void display_all_lates_for_all_in_sprint()
